9a5d2711ef1749292fa16e86af125952d7360b93
Serhii <mr.ivanov.sergei.iv@gmail.com>
2024-12-25 01:48:52 +0200

    Separation of swap & sort functions from main to source.cpp

---
+++ Header.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void swap(int& i1, int& i2);
+
+void sort(int& i1, int& i2, int& i3);
--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
+#include "Header.h"
 
 using namespace std;
-
-void swap(int& i1, int& i2) {
-	int i_aux = i1;
-	i1 = i2;
-	i2 = i_aux;
-}
-
-void sort(int& i1, int& i2, int& i3) {
-	if (i1 > i2) swap(i1, i2);
-	if (i2 > i3) swap(i2, i3);
-	if (i1 > i2) swap(i1, i2);
-}
 
 int main() {
 	int a;
